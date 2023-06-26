fix tanh running for no normalization

cam_components/agent/creator_funcs.py
@@ -117,7 +117,7 @@ def cam_creator_step(cam_algorithm, model, target_layer, dataset, num_classes:in
                 # --------------------------------------  cam evaluate  -------------------------------------- #
                 if eval_func == 'corr':
                     grayscale_cam = np.array(grayscale_cam)  # # grayscale_cam -- 16 * [1, 256, 256] - batch * [1, 256, 256]
-                    if tanh_flag and im:
+                    if tanh_flag and data_max_value:
                         para_k = (np.arctanh(t_max) - np.arctanh(t_min))/(data_max_value-data_min_value)
                         para_b = (np.arctanh(t_max)*data_min_value-np.arctanh(t_min)*data_max_value)/(data_min_value-data_max_value)
                         grayscale_cam = (np.arctanh(grayscale_cam) - para_b)/para_k

cam_components/core/base_cam_predictor.py
@@ -56,6 +56,9 @@ class BaseCAM_P:
         if self.tanh_flag and self.value_max:
             self.para_k = (np.arctanh(t_max) - np.arctanh(t_min))/(self.value_max-self.value_min)
             self.para_b = (np.arctanh(t_max)*self.value_min-np.arctanh(t_min)*self.value_max)/(self.value_min-self.value_max)
+        else:
+            self.para_k = None
+            self.para_b = None
 
 
     """ Get a vector of weights for every channel in the target layer.
@@ -252,10 +255,11 @@ class BaseCAM_P:
             if self.remove_minus_flag:
                 cam = np.maximum(cam, 0)
             for cam_item in cam:  # from cam[batch, groups, depth, length, width] to [groups, depth, length, width]
-                if self.tanh_flag:
+                if self.tanh_flag and self.para_k:
                     scaled = self.tanh_scale_cam_image(cam_item, target_size, prob_weights)
                 else:
-                    scaled = self.scale_cam_image(cam_item, target_size, prob_weights)  # 放缩不需要改变，不只是放缩比例，更重要的是进行了归一化
+                    scaled = self.scale_cam_image(cam_item, target_size, prob_weights)
+                # 放缩不需要改变，不只是放缩比例，更重要的是进行了归一化
                 # print('max of scaled cam:', np.max(np.asarray(scaled)))
                 # print('min of scaled cam:', np.min(np.asarray(scaled)))
                 # scaled [groups, length, width] / 3D scaled [groups, depth, length, width]